6433fb44b0fe0e1ae25f2f27c14ba30440f2a352
Pranab Amatya <pranab.aamtya.pa@gmail.com>
2021-09-16 19:11:37 +1000

    First

---
+++ src/components/BookList.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1> {{title}} </h1>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'BookList',
+        data() {
+            title: 'All Books'
+        }
+    };
+</script>
+<style>
+    h1, h1 {
+        font-weight: normal;
+    }
+</style>
